e23b38a24061569b03af29a987fbf8254951cc7c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-31 16:10:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction_amount
+  
+  
+  def initialize(discount=0)
+  @total = 0 #sets an instance variable @total on initialization to zero optionally takes an employee discount on initialization
+  @discount = discount
+  @items = []
+  end
+  
+  def add_item(title,price,quantity=0) #accepts a title and price and increases the total #also accepts an optional quantity
+     quantity > 1 ? self.total += (price * quantity) : self.total += price
+    
+    while quantity > 0 
+      items.push(title)
+      quantity -= 1 
+    end
+    # doesn't forget about the previous total
+    @last_transaction = quantity
+  end
+  
+  def apply_discount
+    self.discount = 0
+end
+end
