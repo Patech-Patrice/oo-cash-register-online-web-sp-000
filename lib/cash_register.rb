24f166d197dfcd11abc95bfd6bd1772b043d2d31
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-31 16:11:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,25 +1,42 @@
 class CashRegister
-  attr_accessor :total, :discount, :items, :last_transaction_amount
-  
-  
-  def initialize(discount=0)
-  @total = 0 #sets an instance variable @total on initialization to zero optionally takes an employee discount on initialization
-  @discount = discount
-  @items = []
+
+  attr_accessor :total, :discount, :last_transaction_amount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
-  
-  def add_item(title,price,quantity=0) #accepts a title and price and increases the total #also accepts an optional quantity
-     quantity > 1 ? self.total += (price * quantity) : self.total += price
-    
-    while quantity > 0 
-      items.push(title)
-      quantity -= 1 
+
+
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
     end
-    # doesn't forget about the previous total
-    @last_transaction = quantity
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
   end
-  
-  def apply_discount
-    self.discount = 0
-end
+
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
+
 end
